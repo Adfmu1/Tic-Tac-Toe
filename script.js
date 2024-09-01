6051e92646152d488cbf49c63f8e8016008a5628
Adfmu <Adfmu3@gmail.com>
2024-09-01 23:27:05 +0200

    add gameboard factory variable with gameboard display

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Im working");
+
+const gameboard = (function createGameboard() {
+    let gameboard = {
+        1: [""],
+        2: [""],
+        3: [""],
+        4: [""],
+        5: [""],
+        6: [""],
+        7: [""],
+        8: [""],
+        9: [""]
+    };
+
+    function displayGameboard() {
+        for (let key in gameboard) {
+            console.log(gameboard[key]);
+            if (key % 3 === 0) {
+                console.log("\n");
+            }
+        }
+    }
+
+    return { displayGameboard };
+})();
