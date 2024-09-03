177dbca6ba68c03471f360a5d2794c16826acee1
Adfmu <Adfmu3@gmail.com>
2024-09-03 21:11:49 +0200

    create game controller which check for winner

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-console.log("Im working");
-
 const gameboard = (() => {
     let gameboard = {
         1: "",
-        2: "",
+        2: "X",
         3: "",
-        4: "",
+        4: "X",
         5: "",
         6: "",
-        7: "",
-        8: "",
-        9: ""
+        7: "X",
+        8: "X",
+        9: "X"
     };
 
     function displayGameboard() {
+        let row = "";
         for (let key in gameboard) {
-            console.log(gameboard[key]);
+            row += gameboard[key];
             if (key % 3 === 0) {
-                console.log("\n");
+                console.log(row);
+                row = "";
             }
         }
+        
+        gameController.checkForWinner(gameboard);
     }
 
     function changeField( chosenField, playerName ) {
@@ -29,6 +31,8 @@ const gameboard = (() => {
         else if (playerName === "playerO") {
             gameboard[chosenField] = "O"
         }
+
+        gameController.checkForWinner(gameboard);
     }
 
     return { gameboard, displayGameboard, changeField };
@@ -60,3 +64,37 @@ const playerO = (() => {
     };
 })();
 
+const gameController = (() => {
+    function checkForWinner(gameboard) {
+        const winConditions = [
+                                [1, 2, 3],
+                                [1, 4, 7],
+                                [1, 5, 9],
+                                [2, 5 ,],
+                                [3, 6, 9],
+                                [3, 5, 7],
+                                [4, 5, 6],
+                                [7, 8, 9]
+                            ];
+        let checkString = "";
+                            
+        // for each condition in winConditions
+        for (let i = 0; i < 8; i++) {
+            // check for each index in winConditions
+            for (let j = 0; j < 3; j++) {
+                checkString += gameboard[winConditions[i][j]];
+            }
+
+            if (checkString === "XXX") {
+                console.log("PlayerX wins");
+            }
+            else if (checkString === "OOO") {
+                console.log("PlayerO wins");
+            }
+
+            checkString = "";
+        }
+    }
+
+    return { checkForWinner }
+})();
